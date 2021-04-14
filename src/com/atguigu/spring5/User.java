d19d9b71a0769023bb0511be89df98ea3c0ba5a9
wangliping <374422089@qq.com>
2021-04-14 15:14:18 +0800

    spring项目

---
+++ src/com/atguigu/spring5/User.java
@@ -0,0 +1,11 @@
+package com.atguigu.spring5;
+
+/**
+ * @author wlp
+ * @create 2021-01-25 21:14
+ */
+public class User {
+    public void add(){
+        System.out.println("add......");
+    }
+}
